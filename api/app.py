5f57c687245770afaab29c31fb69fd2dc9ef113e
Dídac Ortega Escalera <didacoe@gmail.com>
2024-02-21 15:14:16 +0100

    Connection testing with firebase database

--- api/app.py
+++ api/app.py
@@ -1,7 +1,27 @@
-import time
-from flask import Flask
+import firebase_admin
+from firebase_admin import credentials
+from firebase_admin import db
+
+# import json
+from typing import Any
+
+from flask import Flask, g, request
+# from flask_cors import CORS
+
+cred = credentials.Certificate('local/cred.json')
+firebase_admin.initialize_app(cred, {'databaseURL': 'https://chat-web-vue-flask-default-rtdb.europe-west1.firebasedatabase.app/'})
+
+ref = db.reference('/users')
+
 app = Flask(__name__)
 
-@app.route('/time')
-def get_current_time():
-    return {'time': time.time()}
+@app.route('/users')
+def get_users():
+    return ref.get()
+
+@app.route('/')
+def hello_world():
+    return ref.get()
+
+if __name__ == '__main__':
+    app.run(debug=True)
